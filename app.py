efc93f3f4a737f392ed6e84b7eb06444dea8f08b
cponeill <caseyoneill78@hotmail.com>
2017-02-02 21:23:12 +0000

    Keeping server code updated to Pep8 standards.

--- app.py
+++ app.py
@@ -5,44 +5,55 @@ from flask import Flask, render_template, jsonify
 import os
 from scraper import get_data
 
+
 app = Flask(__name__)
+
 
 # The following functions all render to their corresponding HTML page and file.
 @app.route("/")
 def hello():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/focus")
 def about():
-	return render_template("focus.html")
-	
+    return render_template("focus.html")
+
+
 @app.route("/skills")
 def skills():
-	return render_template("skills.html")
+    return render_template("skills.html")
+
 
 @app.route("/projects")
 def projects():
-	return render_template("projects.html")
-	
+    return render_template("projects.html")
+
+
 @app.route("/projects/marketing")
 def marketing():
-	return render_template("marketing.html")
+    return render_template("marketing.html")
+
 
 @app.route("/projects/development")
 def development():
-	return render_template("development.html")
+    return render_template("development.html")
+
 
 @app.route("/api")
 def api():
-	return render_template("api.html")
+    return render_template("api.html")
+
 
 @app.route("/api/data.json")
 def api_return():
-	return jsonify(get_data())
-	
+    return jsonify(get_data())
+
+
 @app.route('/keybase.txt')
 def keybase():
-	return render_template("keybase.txt")
+    return render_template("keybase.txt")
+
 
 if __name__ == "__main__ ":
-	app.run()
+    app.run()
