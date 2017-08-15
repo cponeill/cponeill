5bc0a18da2a733c02cd945482c90af2b730069a9
cponeill <caseyoneill78@hotmail.com>
2017-08-15 13:52:53 +0000

    more CSP tests.

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def keybase():
 
 @app.after_request
 def apply_caching(response):
-    response.headers['Content-Security-Policy'] = "default-src https"
+    response.headers['Content-Security-Policy'] = "default-src self"
     response.headers['Referrer-Policy'] = 'no-referrer-when-downgrade'
     response.headers['X-Xss-Protection'] = '1'
     response.headers['X-Frame-Options'] = 'SAMEORIGIN'
